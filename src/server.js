e9229111c14ad734b9224b7d2bb31a97b7d5088f
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-04-08 13:14:30 -0600

    Se agrego un middelware para la configuracion de los cors

--- src/server.js
+++ src/server.js
@@ -7,6 +7,14 @@ const FavoriteRoutes = require('./routes/Favorite.routes')
 
 const server = express()
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
+
 server.use(express.json())
 server.use(morgan('dev'))
 
